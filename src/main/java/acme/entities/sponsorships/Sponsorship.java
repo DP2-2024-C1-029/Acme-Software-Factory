a1d5fb8a90552028cedaa247aa1b81e2ae4b0683
jose-maria-portela <josporhue@alum.us.es>
2024-03-02 20:28:49 +0100

    [TASK-003] Mandatory - Sponsorship - Student#4  #66

---
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -0,0 +1,80 @@
+
+package acme.entities.sponsorships;
+
+import java.sql.Date;
+import java.time.Duration;
+
+import javax.persistence.Column;
+import javax.persistence.Inheritance;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.datatypes.Money;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Inheritance
+public class Sponsorship {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@NotNull
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@Past
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	// Methods for the duration
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				initialExecutionPeriod;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				endingExecutionPeriod;
+
+	// TODO - Must be positive (in the servicies)
+	@NotNull
+	private Money				amount;
+
+	@NotNull
+	private SponsorType			type;
+
+	@Email
+	@NotNull
+	private String				optionalEmail;
+
+	@URL
+	@NotNull
+	private String				optionalLink;
+
+	// Derived attributes -----------------------------------------------------
+
+
+	// TODO - Validar en servicios mínimo un mes después
+	@Transient
+	private Duration duration() {
+		return Duration.between((java.time.temporal.Temporal) this.initialExecutionPeriod, (java.time.temporal.Temporal) this.endingExecutionPeriod);
+	}
+
+	// Relationships ----------------------------------------------------------
+}
