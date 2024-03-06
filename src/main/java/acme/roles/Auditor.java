abbbbdc51a4ba42ee6d2404bf825321f1b622300
Jaime Varas Cáceres <jaimevarascaceres@gmail.com>
2024-03-06 14:48:15 +0100

    task #54 done

--- src/main/java/acme/roles/Auditor.java
+++ src/main/java/acme/roles/Auditor.java
@@ -4,6 +4,9 @@ package acme.roles;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
 import acme.client.data.AbstractRole;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,6 +27,25 @@ public class Auditor extends AbstractRole {
 
 	@NotBlank
 	private String				sector;
+
+	//	firm (not blank, shorter than 76 characters)
+	@NotBlank
+	@Length(max = 75)
+	private String				firm;
+
+	//	professional ID (not blank, shorter than 26 characters)
+	@NotBlank
+	@Length(max = 25)
+	private String				professionalId;
+
+	//	a list of certifications (not blank, shorter than 101 characters)
+	@NotBlank
+	@Length(max = 100)
+	private String				certifications;
+
+	//	an optional link with further information
+	@URL
+	private String				link;
 
 	// Derived attributes -----------------------------------------------------
 
