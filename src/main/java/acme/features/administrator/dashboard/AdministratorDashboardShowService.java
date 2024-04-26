a6a678b95fc3073ff563a7912529ad5048f75248
lDrakohh <lDrakohh@gmail.com>
2024-04-26 17:43:37 +0200

    docs: Lint Report

--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardShowService.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardShowService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.client.data.accounts.Administrator;
-import acme.client.data.accounts.Principal;
 import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
 import acme.forms.AdministratorDashboard;
@@ -30,8 +29,7 @@ public class AdministratorDashboardShowService extends AbstractService<Administr
 
 	@Override
 	public void load() {
-		int developerId;
-		Principal principal;
+
 		AdministratorDashboard dashboard;
 		Integer totalNumberOfPrincipalsWithAdministrator;
 		Integer totalNumberOfPrincipalsWithManager;
