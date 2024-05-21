846d351948eeace3fd132d2de5a330812a9d2feb
Jaime Varas Cáceres <jaimevarascaceres@gmail.com>
2024-05-21 12:06:50 +0200

    new safe for auditRecord + indexes

--- src/main/java/acme/entities/codeaudits/AuditRecord.java
+++ src/main/java/acme/entities/codeaudits/AuditRecord.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -24,6 +26,10 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "draftMode, code_audit_id"), //
+	@Index(columnList = "code_audit_id"), //
+})
 public class AuditRecord extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
